bf54b143db42d7593f2a4fd6e2e9ffe628e7fff3
SEC-VT <shanec1@vt.edu>
2018-04-23 11:36:55 -0400

    new slimmer filelist generator, grabs remote files as needed
    Signed-off-by: SEC-VT <shanec1@vt.edu>

--- halley.py
+++ halley.py
@@ -1,10 +1,82 @@
 import os
+import requests
 import numpy as np
 import pandas as pd
 import datetime as dt
 
 datapath_local = 'S:/Space/Datasets/halley'
-# datapath_remote = '/home/aalpip/data/'
+datapath_remote = 'http://psddb.nerc-bas.ac.uk/data/psddata/atmos/space/'
+
+
+def fetch_remote(datetime, subsystem='sc'):
+    """Grabs mag data from UK Polar Data Centre, NERC and saves a local copy to datapath_local/{instrument}/{year}/{file}
+
+    Args:
+        datetime (datetime): python datetime object
+        subsystem (str, optional): Instrument string ('scm' or 'fgm')
+
+    Returns:
+        available (bool): True if file is downloaded from remote server
+    """
+    year = datetime.year
+    doy = '{:03}'.format(datetime.timetuple().tm_yday)
+    month = '{:02}'.format(datetime.month)
+    date = '{:02}'.format(datetime.day)
+    URL = {'sc': '{base_url}/scm/halley//{year}/data/ascii/{doy}{year}.TXT'.format(base_url=datapath_remote, year=year, doy=doy),
+           'fg': '{base_url}/fluxgate/halley//{year}/data/00001/ZFM{year}{month}{date}.dat'.format(base_url=datapath_remote, year=year, doy=doy, month=month, date=date)}
+    r = requests.get(URL[subsystem])
+
+    available = r.status_code == requests.codes.ok
+    if available:
+        try:
+            with open('{0}/{3}/{1}/{2}{1}.txt'.format(datapath_local, year, doy, subsystem), 'w+') as local_file:
+                local_file.write(r.text)
+        except FileNotFoundError:
+            try:
+                os.makedirs('{0}/{1}/{2}'.format(datapath_local, subsystem, year))
+                with open('{0}/{3}/{1}/{2}{1}.txt'.format(datapath_local, year, doy, subsystem), 'x+') as local_file:
+                    local_file.write(r.text)
+            except OSError:
+                pass
+            pass
+    return available
+
+
+def generate_filelist(start, end=None, subsystem='sc'):
+    """Search the local and remote datapaths for files in the given date range
+
+    Args:
+        start (datetime): First day of timespan
+        end (datetime, optional): last day of timespan. If None (default) then end = start
+        subsystem (str, optional): Instrument data to search for ('sc' or 'fg')
+
+    Returns:
+        filelist (list): List of string paths to files representing data for the given dates
+    """
+    end = start if end is None else end
+    if (type(start) is dt.datetime) and (type(end) is dt.datetime) and (start < end):
+        searchlist = pd.date_range(start=start, end=end).to_pydatetime().tolist()
+        remotelist = []
+        locallist = []
+        for date in searchlist:
+            year = date.year
+            doy = '{:03}'.format(date.timetuple().tm_yday)
+            try:
+                with open('{0}/{3}/{1}/{2}{1}.txt'.format(datapath_local, year, doy, subsystem), 'x'):
+                    pass
+                remotelist.append(date)
+            except FileExistsError:
+                locallist.append(date)
+            except FileNotFoundError:
+                remotelist.append(date)
+        locallist = locallist + [date for date in remotelist if fetch_remote(date, subsystem=subsystem)]
+
+        filelist = []
+        for date in locallist:
+            year = date.year
+            doy = '{:03}'.format(date.timetuple().tm_yday)
+            filelist.append('{0}/{3}/{1}/{2}{1}.txt'.format(datapath_local, year, doy, subsystem))
+    return filelist
 
 
 def generate_yearly_masterlist(year=2017, subsystem='sc', local=True):
